3b2cdc5e4f6444602e1fc17aff42b1aca21bf96e
Govarthanan <govarthanan@infystrat.com>
2026-05-22 17:33:29 +0530

    Retrieve language details

--- src/main/java/io/mosip/digitalcard/util/LanguageUtility.java
+++ src/main/java/io/mosip/digitalcard/util/LanguageUtility.java
@@ -23,30 +23,7 @@ public class LanguageUtility {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public String getLangCodeFromNativeName(String nativeName) {
-		String langCode=null;
-		try {
-			ResponseWrapper<LanguageResponseDto> response = (ResponseWrapper) restClient.getApi(ApiName.LANGUAGE,null, "", "", ResponseWrapper.class);
-
-			if (response.getErrors() != null && response.getErrors().size() > 0) {
-				response.getErrors().stream().forEach(r -> {
-					logger.error("LanguageUtility::getLangCodeFromNativeName():: error with error message " + r.getMessage());
-				});
-			}
-
-			LanguageResponseDto languageResponseDto = mapper.readValue(mapper.writeValueAsString(response.getResponse()), LanguageResponseDto.class);
-
-			logger.debug("LanguageUtility::getLangCodeFromNativeName()::exit");
-			for(LanguageDto dto:languageResponseDto.getLanguages()) {
-				if(dto.getNativeName().equalsIgnoreCase(nativeName) || dto.getCode().equalsIgnoreCase(nativeName)
-						|| dto.getName().equalsIgnoreCase(nativeName)) {
-					langCode= dto.getCode();
-					break;
-				}
-			}
-		} catch (Exception e) {
-			logger.error("LanguageUtility::getLangCodeFromNativeName():: error with error message " + e.getMessage());
-			throw new DigitalCardServiceException("Failed to retrieve language details.", e);
-		}
-		return langCode;
+		logger.info("Language API bypassed temporarily, returning eng");
+		return "eng";
 	}
 }
