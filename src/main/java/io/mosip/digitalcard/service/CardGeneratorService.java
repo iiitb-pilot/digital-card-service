1d646cfe7916ee83bbb8fbc64465c5debd266c42
Govarthanan <govarthanan@infystrat.com>
2026-07-16 18:33:59 +0530

    Updated Implementaion of INJI VC

--- src/main/java/io/mosip/digitalcard/service/CardGeneratorService.java
+++ src/main/java/io/mosip/digitalcard/service/CardGeneratorService.java
@@ -14,8 +14,9 @@ public interface CardGeneratorService {
 	 * @param credentialType
 	 * @param password
 	 * @param templateLang
+	 * @param vc
 	 * @return
 	 */
-	public byte[] generateCard(JSONObject decryptedCredentialJson, String credentialType, String password, Map<String,Object> additionalAttributes, String templateLang) throws Exception;
+	public byte[] generateCard(JSONObject decryptedCredentialJson, String credentialType, String password, Map<String,Object> additionalAttributes, String templateLang,String vc) throws Exception;
 
 }
