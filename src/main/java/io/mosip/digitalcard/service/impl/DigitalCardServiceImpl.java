a8983f010020297d2845a47cd7116b39439a8a23
Govarthanan <govarthanan@infystrat.com>
2026-07-22 17:39:36 +0530

    DCimpl service and Pom changes for Inji Vc implementaion

--- src/main/java/io/mosip/digitalcard/service/impl/DigitalCardServiceImpl.java
+++ src/main/java/io/mosip/digitalcard/service/impl/DigitalCardServiceImpl.java
@@ -142,6 +142,7 @@ public class DigitalCardServiceImpl implements DigitalCardService {
             decryptedCredential = encryptionUtil.decryptData(credential);
             JSONObject jsonObject = new org.json.JSONObject(decryptedCredential);
             JSONObject decryptedCredentialJson = jsonObject.getJSONObject("credentialSubject");
+            logger.info("DECRYPTED JSON RESPONSE {}", decryptedCredentialJson);
             rid=getRid(decryptedCredentialJson.get("id"));
             attributes.put(IdType.RID.toString(), rid);
             //sets additional attributes for all templates.
