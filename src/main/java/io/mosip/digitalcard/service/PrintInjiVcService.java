aa1bd1b5241c92a344fcf7df4f9cdb621680203b
Govarthanan <govarthanan@infystrat.com>
2026-07-27 18:01:34 +0530

    Inji Vc implementaion for mec-1.3.0 was pushing

---
+++ src/main/java/io/mosip/digitalcard/service/PrintInjiVcService.java
@@ -0,0 +1,9 @@
+package io.mosip.digitalcard.service;
+import java.util.Map;
+public interface PrintInjiVcService {
+    String generatePreAuthorizedCode(Map<String, Object> claims);
+    String getCredentialOffer(String offerId);
+    String exchangeCodeForToken(String preAuthorizedCode);
+    String issueCredential(String accessToken, String cNonce);
+    String generateProofJwt(String cNonce);
+}
